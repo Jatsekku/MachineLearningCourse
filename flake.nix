2e51b3abf5b923621704b5cc4ab555d6d65acc28
Jatsekku <jatsekku2@gmail.com>
2025-10-21 00:53:56 +0200

    feat(nix): Add dev flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "JupyterLab dev shell using Nix Flakes";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          ipympl
+          jupyterlab
+          torch
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+          ];
+        };
+      });
+}
+
